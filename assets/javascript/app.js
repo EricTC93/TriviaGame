d8d5319d416a45769354a64d882a25dd5881e328
YETC7 <tyler_726@live.com>
2017-05-23 21:59:15 -0700

    start questionList object

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var questionList = [
+	{
+		question: "What says Yes?",
+		possibleAnswers: ["no","nope","yes","no way"],
+		answer: 2
+	},
+
+	{
+		question: "What says Yes?",
+		possibleAnswers: ["no","nope","null","yes"],
+		answer: 3
+	}
+
+];

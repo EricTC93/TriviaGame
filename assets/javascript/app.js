27a6ef358137f0bed5aef89489ff5a770e8eed9c
YETC7 <tyler_726@live.com>
2017-05-24 19:24:26 -0700

    add timer to count 30 sec.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,13 @@
+// Create a list of questions
+// User clicks start button
+// Display question to the user and timer
+// User attemps to answer the question with a click
+// Tells the user if they are correct or incorrect
+// Moves to the next question with a new timer
+// Ends the game after a certain amount of questions
+// Reset
+
+
 var questionList = [
 	{
 		question: "What says Yes?",
@@ -23,3 +33,18 @@ $("#answerD").text(questionList[0].possibleAnswers[3]);
 $(".answer").on("click",function() {
 	console.log($(this).text());
 });
+
+var timer = 30;
+var intervalId;
+$("#timeRemaining").text(timer);
+intervalId = setInterval(countdown,1000);
+
+function countdown () {
+	timer--;
+	$("#timeRemaining").text(timer);
+
+	if (timer === 0) {
+		clearInterval(intervalId);
+	}
+}
+
